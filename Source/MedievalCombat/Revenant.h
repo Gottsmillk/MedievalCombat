30b367dfe27333298b28cefc955a4083836b404e
Gottsmillk <daniellee0217@yahoo.com>
2017-06-05 20:03:21 -0400

    Added Revenant C++ class as child of BaseCharacter C++ class and parent of Revenant BP Cleaned up temporary functions and variables from Revenant BP

---
+++ Source/MedievalCombat/Revenant.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BaseCharacter.h"
+#include "Revenant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIEVALCOMBAT_API ARevenant : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
